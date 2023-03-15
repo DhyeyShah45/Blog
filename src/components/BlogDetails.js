454c034873e7518ab63931b48dfb4b1762d5fefc
Dhyey Shah <dhyey.shah45@gmail.com>
2023-03-15 14:43:46 +0530

    edit feature is live

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -3,53 +3,66 @@ import { useAuthContext } from "../hooks/useAuthContext";
 import delete_icon from "../delete.png";
 import edit_icon from "../pen.png";
 import useFetch from "../hooks/useFetch";
+import EditForm from "./EditForm";
+import { useState } from "react";
 const BlogDetails = () => {
   const { user } = useAuthContext();
   const url = "/blog/";
   const { id } = useParams();
+  const [permission, setPermission] = useState(false);
 
   const { data: blog, isPending, error } = useFetch(url + id);
-  const history = useNavigate();
+  const navigate = useNavigate();
 
   const handleDelete = () => {
-    fetch("/blog/" + blog._id, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${user.token}`,
-      },
-    }).then((res) => {
-      if(!res){
-        history("/");
-      }
-      alert("You can only delete your blogs")
-
-    });
+    if (blog.author === user.name) {
+      fetch("/blog/" + blog._id, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      }).then(() => {
+        navigate("/");
+      });
+    } else {
+      alert("You can only delete your blogs");
+    }
   };
-  const handleEdit = () => {};
+  const handleEdit = () => {
+    if (blog.author === user.name) {
+      setPermission(true);
+    } else {
+      alert("You can edit your blog only!");
+    }
+  };
   return (
-    <div className="blog-details">
-      {error && <div>{error}</div>}
-      {isPending && <div>Loading...</div>}
-      {blog && (
-        <article>
-          <div className="blog-details-header">
-            <div>
-              <h2>{blog.title}</h2>
-              <p>{blog.author}</p>
+    <>
+      <div className="blog-details">
+        {}
+        {error && <div>{error}</div>}
+        {isPending && <div>Loading...</div>}
+        {blog && (
+          <article>
+            <div className="blog-details-header">
+              <div>
+                <h2>{blog.title}</h2>
+                <p>{blog.author}</p>
+              </div>
+              <div className="action">
+                <button onClick={handleEdit}>
+                  <img src={edit_icon} alt="Edit Icon" width="30px" />
+                </button>
+                <button onClick={handleDelete}>
+                  <img src={delete_icon} alt="Delete Icon" width="30px" />
+                </button>
+              </div>
             </div>
-            <div className="action">
-              <button onClick={handleEdit}>
-                <img src={edit_icon} alt="Edit Icon" width="30px" />
-              </button>
-              <button onClick={handleDelete}>
-                <img src={delete_icon} alt="Delete Icon" width="30px" />
-              </button>
-            </div>
-          </div>
-          <div>{blog.body}</div>
-        </article>
-      )}
-    </div>
+            <div>{blog.body}</div>
+          </article>
+        )}
+        {permission && <EditForm blog={blog} token={user.token} />}
+      </div>
+    </>
   );
 };
 
