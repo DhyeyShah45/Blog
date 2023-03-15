4a8298a113c548c41ab7bd29e477f1fab640a0b6
Dhyey Shah <dhyey.shah45@gmail.com>
2023-03-15 12:10:30 +0530

    delete only personal blog

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,35 +1,52 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
-
+import delete_icon from "../delete.png";
+import edit_icon from "../pen.png";
 import useFetch from "../hooks/useFetch";
 const BlogDetails = () => {
-  const {user} = useAuthContext()
+  const { user } = useAuthContext();
   const url = "/blog/";
-  const {id} = useParams();
+  const { id } = useParams();
 
   const { data: blog, isPending, error } = useFetch(url + id);
   const history = useNavigate();
 
-  const handleClick = () => {
+  const handleDelete = () => {
     fetch("/blog/" + blog._id, {
       method: "DELETE",
       headers: {
-        'Authorization': `Bearer ${user.token}`
+        Authorization: `Bearer ${user.token}`,
+      },
+    }).then((res) => {
+      if(!res){
+        history("/");
       }
-    }).then(() => {
-      history("/");
+      alert("You can only delete your blogs")
+
     });
   };
+  const handleEdit = () => {};
   return (
     <div className="blog-details">
       {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blog && (
         <article>
-          <h2>{blog.title}</h2>
-          <p>{blog.author}</p>
+          <div className="blog-details-header">
+            <div>
+              <h2>{blog.title}</h2>
+              <p>{blog.author}</p>
+            </div>
+            <div className="action">
+              <button onClick={handleEdit}>
+                <img src={edit_icon} alt="Edit Icon" width="30px" />
+              </button>
+              <button onClick={handleDelete}>
+                <img src={delete_icon} alt="Delete Icon" width="30px" />
+              </button>
+            </div>
+          </div>
           <div>{blog.body}</div>
-          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -4,6 +4,8 @@ const BlogList = ({blogs,title,handleDelete}) => {
   return ( 
     <div className="blog-list">
       <h2>{title}</h2>
+      {/* show only if no blog present */}
+      {!blogs.length && <div>Please add some blogs..</div>}
       {blogs.map((blog)=>(
           <div className="blog-preview" key={blog._id}>
             {/* {console.log(blog._id)} */}
